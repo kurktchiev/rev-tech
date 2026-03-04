0a4f5b0b6c30e6c82621d060036088cfe6ad17d2
Jasmit Tarang <jaytarang92@gmail.com>
2026-03-04 13:07:33 -0600

    containerize the frontend app (#8)
    * containerize the frontend app
    
    * env var variables
    
    * Add all variables to .env.example, no more optional vars
    
    * Uncomment JWT handler
    
    * Add MCP functionality via config file
    
    * Add only the env vars required
    
    * Remove CHAINLIT_AUTH_SECRET from env. Set it to None in the Dockerfile
    
    * Remove CHAINLIT_AUTH_SECRET from docker-compose
    
    * Allow tbot and tsh only for STDIO servers
    
    * Normalize env vars for chainlit
    
    * Add the smallest possible model to run in the cloud
    
    ---------
    
    Co-authored-by: Boris 'B' Kurktchiev <kurktchiev@gmail.com>

--- use-cases/ai/frontend/main.py
+++ use-cases/ai/frontend/main.py
@@ -4,9 +4,11 @@ from typing import Optional, Dict
 import chainlit as cl
 import httpx
 from mcp import ClientSession
+from os import environ
 
-OLLAMA_URL = "http://localhost:11434/v1/chat/completions"
-MODEL = "qwen2.5:7b"
+
+CHAT_COMPLETION_URL = environ.get("CHAINLIT_CHAT_COMPLETION_URL")
+CHAT_MODEL = environ.get("CHAINLIT_CHAT_MODEL")
 
 SYSTEM_PROMPT = (
     "You are a helpful assistant. "
@@ -151,7 +153,7 @@ async def main(message: cl.Message):
             tool_called = False
 
             payload = {
-                "model": MODEL,
+                "model": CHAT_MODEL,
                 "messages": messages,
                 "stream": True,
             }
@@ -161,7 +163,7 @@ async def main(message: cl.Message):
                 payload["tool_choice"] = "auto"
 
             response = await client.post(
-                OLLAMA_URL,
+                CHAT_COMPLETION_URL,
                 json=payload,
                 headers={"Content-Type": "application/json"},
             )
